982e501a7e6e6571ef5aab2632e3519d7edbadd7
Vikas Verma <vikasverma15@gmail.com>
2026-01-04 19:05:21 +0530

    correct

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Form from "./components/Form";
-import FormRef from "./components/FormRef";
-import FormState from "./components/FormState";
+import Form from "./Form";
+import FormRef from "./FormRef";
+import FormState from "./FormState";
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <nav>
-          <Link id="form-link" to="/">Form</Link>{" | "}
-          <Link id="form-ref-link" to="/ref">Form Ref</Link>{" | "}
-          <Link id="form-state-link" to="/state">Form State</Link>
-        </nav>
+      <nav>
+        <Link id="form-link" to="/">Form</Link>{" | "}
+        <Link id="form-ref-link" to="/ref">Form Ref</Link>{" | "}
+        <Link id="form-state-link" to="/state">Form State</Link>
+      </nav>
 
-        <Routes>
-          <Route path="/" element={<Form />} />
-          <Route path="/ref" element={<FormRef />} />
-          <Route path="/state" element={<FormState />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/ref" element={<FormRef />} />
+        <Route path="/state" element={<FormState />} />
+      </Routes>
     </Router>
   );
 };
 
 export default App;
+
